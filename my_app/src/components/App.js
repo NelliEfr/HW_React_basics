8e78dbcc1fab1d72b194dd972e6d362a3ed86dd8
AndreasUglianski <a.uglyansky@gmail.com>
2022-10-26 09:48:03 +0200

    object added

--- my_app/src/components/App.js
+++ my_app/src/components/App.js
@@ -1,22 +1,23 @@
 import User from './User';
-const users = [
-	{
-		id: 1,
-		firstname: 'Oleg',
-		lastname: 'Petrov',
-		address: 'Moscow, Russia',
-		salary: 280,
-	},
-	{
-		id: 2,
-		firstname: 'Anna',
-		lastname: 'Ivanova',
-		address: 'Berlin, Germany',
-		salary: 500,
-	},
-];
 
 function App() {
+	const users = [
+		{
+			id: 1,
+			firstname: 'Oleg',
+			lastname: 'Petrov',
+			address: 'Moscow, Russia',
+			salary: 280,
+		},
+		{
+			id: 2,
+			firstname: 'Anna',
+			lastname: 'Ivanova',
+			address: 'Berlin, Germany',
+			salary: 500,
+		},
+	];
+
 	return (
 		<div>
 			{users.map((el) => (
